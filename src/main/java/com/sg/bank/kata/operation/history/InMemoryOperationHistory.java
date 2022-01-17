2b2ce3284d4f2f7b9536f905968b91b63f65b68a
Karim Goubbaa <karim.goubaa@gmail.com>
2022-01-17 22:47:30 +0100

    feat(account): implement account operations

--- src/main/java/com/sg/bank/kata/operation/history/InMemoryOperationHistory.java
+++ src/main/java/com/sg/bank/kata/operation/history/InMemoryOperationHistory.java
@@ -1,19 +1,35 @@
 package com.sg.bank.kata.operation.history;
 
+import com.sg.bank.kata.operation.Operation;
 import com.sg.bank.kata.operation.OperationType;
 import com.sg.bank.kata.statement.Statement;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class InMemoryOperationHistory implements OperationHistory {
+    private final List<Operation> operations = new LinkedList();
 
     @Override
     public void save(OperationType operationType, BigDecimal amount) {
+        if (operationType == OperationType.WITHDRAW) {
+            amount = amount.negate();
+        }
+        this.operations.add(new Operation(operationType, LocalDateTime.now(), amount));
     }
 
     @Override
     public List<Statement> getStatements() {
-        return null;
+        List<Statement> statements = new LinkedList();
+        AtomicReference<BigDecimal> balance = new AtomicReference<>(BigDecimal.valueOf(0));
+        operations.stream()
+                .forEach(operation -> {
+                    balance.accumulateAndGet(balance.get().add(operation.getAmount()), (a, b) -> a.add(b));
+                    statements.add(new Statement(operation, balance.get()));
+                });
+        return statements;
     }
 }
--- src/main/java/com/sg/bank/kata/statement/ConsoleStatementPrinter.java
+++ src/main/java/com/sg/bank/kata/statement/ConsoleStatementPrinter.java
@@ -1,9 +1,33 @@
 package com.sg.bank.kata.statement;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class ConsoleStatementPrinter implements StatementPrinter {
+
+    private static final String FIELD_SEPARATOR = " | ";
+    private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
     @Override
     public void print(List<Statement> statements) {
+        if (!statements.isEmpty()) {
+            printHeaders();
+            statements.stream().forEach(this::printStatement);
+        }
+    }
+
+    private void printHeaders() {
+        System.out.println("OPERATION | DATE | AMOUNT | BALANCE");
+    }
+
+    private void printStatement(Statement statement) {
+        System.out.println(statement.getOperation().getOperationType().name()
+                + FIELD_SEPARATOR
+                + dateFormatter.format(statement.getOperation().getDate())
+                + FIELD_SEPARATOR
+                + statement.getOperation().getAmount()
+                + FIELD_SEPARATOR
+                + statement.getBalance()
+        );
     }
 }
